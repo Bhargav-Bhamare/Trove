a83a2544b09f777d4e1aa2817ed56408bb5ea5e7
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-05-20 21:56:41 +0530

    More of Backend Work

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+// const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+
+//Establishing Connection
+main()
+.then(() => console.log("DB Connection Successful!"))
+.catch(err => console.log(err));
+
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/trove");
+};
+
+
 
 //Middlewares
 app.set("view engine","ejs");
@@ -10,6 +31,44 @@ app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(methodOverride("_method"));
+
+// MongoStore Options
+const MongoStore = require("connect-mongo");
+
+const store = new MongoStore({
+    mongoUrl: "mongodb://localhost:27017/trove",
+    touchAfter: 24 * 3600,
+});
+store.on("error", (err)=>{
+    console.log("Error in MONGO SESSION STORE!",err);
+});
+
+app.use(require("express-session")({
+    secret: "ThisIsASecret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport Configuration
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Middleware to check if user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash("error", "You must be signed in to access this page");
+    res.redirect("/signin");
+};
 
 
 // Landing Page
@@ -19,6 +78,34 @@ app.get("/", (req,res) =>{
 
 app.get("/signin", (req,res) =>{
     res.render("signin.ejs");
+});
+
+app.post("/signin", passport.authenticate("local", {failureRedirect: "/signin", failureFlash: true}), (req,res) =>{
+    res.redirect("/home");
+});
+
+app.post("/signup", async (req,res) =>{
+    try{
+        let { username,email,password} = req.body;
+        const newUser = new User({email,username});
+        const registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        req.login(registeredUser,(err) =>{
+            if(err){
+                return res.redirect("/signin");
+            }
+            req.flash("success","User registered Successfully");
+            res.redirect("/home");
+        });  
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signin");
+    }
+});
+
+
+app.get("/home", isLoggedIn, (req,res) =>{
+    res.render("home.ejs",{user: req.user});
 });
 
 //Server Check
